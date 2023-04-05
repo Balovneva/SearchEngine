b2c782d8ceea59a116df8a2a5fefd50fc6e4c379
@mariia_skorobogatova <maria.skor96@gmail.com>
2023-04-05 13:32:26 +0300

    Variables have been changed in lemma finder

--- src/main/java/searchengine/controllers/ApiController.java
+++ src/main/java/searchengine/controllers/ApiController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import searchengine.dto.search.SearchResponse;
 import searchengine.dto.statistics.StatisticsResponse;
 import searchengine.services.SearchService;
 import searchengine.services.SiteIndexingService;
